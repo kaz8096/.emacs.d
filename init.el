6b7b64cfa35679af013f5e2fccc2ba15dc519eff
foobar <f@bar.com>
2017-11-28 06:06:23 +0000

    update

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'org-install)
-(defvar personal-config-dir (concat user-emacs-directory "personal-config/"))
-(org-babel-load-file (expand-file-name "init.org" personal-config-dir))
+(defvar startup-dir (concat user-emacs-directory "startup.d/"))
+(org-babel-load-file (expand-file-name "init.org" startup-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (helm-ls-git helm helm-swoop))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
