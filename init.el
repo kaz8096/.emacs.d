8bccb4b3ab7ac1e9551ead30128e4d8d45eab347
foobar <f@bar.com>
2017-12-01 04:07:30 +0000

    update org/junk-file

--- init.el
+++ init.el
@@ -8,3 +8,20 @@
 (require 'org-install)
 (defvar startup-dir (concat user-emacs-directory "startup.d/"))
 (org-babel-load-file (expand-file-name "init.org" startup-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(helm-ff-auto-update-initial-value nil)
+ '(helm-gtags-fuzzy-match t)
+ '(helm-gtags-ignore-case t)
+ '(package-selected-packages
+   (quote
+    (fold-dwim fold-this open-junk-file w3m smartrep rainbow-delimiters multiple-cursors multi-web-mode json-mode js2-mode helm-swoop helm-ls-git helm-gtags))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
